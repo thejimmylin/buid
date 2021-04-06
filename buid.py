1a258063e68e3b01da1a2b53fccf7e232a87dcd0
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-04-06 14:39:42 +0800

    add more args

--- buid.py
+++ buid.py
@@ -9,11 +9,14 @@ class BUID:
     CONSONAT_CHARS = 'bcdfghjklmnpqrstvwxyz'
     SEPARATOR_CHARS = '-'
 
-    def __init__(self, phrases=None, format='CVCV-CVCV-CVCV-CVCV'):
+    def __init__(self, phrases=None, format='CVCV-CVCV-CVCV-CVCV', VOWEL_CHARS=None, CONSONAT_CHARS=None, SEPARATOR_CHARS=None):
         if not format:
-            raise ValueError("The format can not be empty.")
+            raise ValueError("The format can't be empty.")
+        self.VOWEL_CHARS = VOWEL_CHARS if VOWEL_CHARS else self.VOWEL_CHARS
+        self.CONSONAT_CHARS = CONSONAT_CHARS if CONSONAT_CHARS else self.CONSONAT_CHARS
+        self.SEPARATOR_CHARS = SEPARATOR_CHARS if SEPARATOR_CHARS else self.SEPARATOR_CHARS
         self.format = format
-        self.phrases = phrases if phrases else self.get_phrases(format=format)
+        self.phrases = self.get_phrases(phrases=phrases, format=self.format)
         if len(self.phrases) != len(self.format):
             raise ValueError('The lengths of the given phrases and format do not match.')
         for f, p in zip(self.format, self.phrases):
@@ -26,7 +29,9 @@ class BUID:
             else:
                 raise ValueError(f'{f} is not a known format character.')
 
-    def get_phrases(self, format):
+    def get_phrases(self, phrases, format):
+        if phrases:
+            return phrases
         phrases = ''
         for f in format:
             if f == self.VOWEL:
